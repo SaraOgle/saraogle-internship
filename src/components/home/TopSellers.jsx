0e26c4fe129dfd42d832d6219808e41f286baaac
SaraOgle <259494087+SaraOgle@users.noreply.github.com>
2026-06-30 21:12:18 -0700

    TopSeller

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from "axios";
 
 const TopSellers = () => {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); 
+
+  async function getUsers() {
+    setLoading(true);
+    try {
+      const { data } = await axios.get("https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers");
+      setUsers(data);
+    } catch (error) {
+      console.error("Error fetching collections:", error);
+    } finally {
+      setLoading(false); 
+    }
+  }
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
@@ -15,24 +34,43 @@ const TopSellers = () => {
           </div>
           <div className="col-md-12">
             <ol className="author_list">
-              {new Array(12).fill(0).map((_, index) => (
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to="/author">
-                      <img
-                        className="lazy pp-author"
-                        src={AuthorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to="/author">Monica Lucas</Link>
-                    <span>2.1 ETH</span>
-                  </div>
-                </li>
-              ))}
+              {loading ? (
+  new Array(6).fill(0).map((_, index) => (
+    <li key={index}>
+      <div className="author_list_pp">
+        <div
+          className="skeleton"
+          style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+        ></div>
+      </div>
+      <div className="author_list_info">
+        <div
+          className="skeleton"
+          style={{ width: "100px", height: "20px", marginBottom: "8px" }}
+        ></div>
+        <div
+          className="skeleton"
+          style={{ width: "60px", height: "15px" }}
+        ></div>
+      </div>
+    </li>
+  ))
+) : (
+  users.map((users) => (
+    <li key={users.id}>
+      <div className="author_list_pp">
+        <Link to={`/author/${users.authorId}`}>
+          <img className="lazy pp-author" src={users.authorImage} alt="" />
+          <i className="fa fa-check"></i>
+        </Link>
+      </div>
+      <div className="author_list_info">
+        <Link to={`/author/${users.authorId}`}>{users.authorName}</Link>
+        <span>{users.price} ETH</span>
+      </div>
+    </li>
+  ))
+)}
             </ol>
           </div>
         </div>
