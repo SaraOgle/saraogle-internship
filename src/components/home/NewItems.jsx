568cb9a25242d73d581691af4450eb8d6125253b
SaraOgle <259494087+SaraOgle@users.noreply.github.com>
2026-06-30 20:53:38 -0700

    New Items

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import OwlCarousel from "react-owl-carousel";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+import Items from "../UI/Items";
 
 const NewItems = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems")
+      .then((res) => {
+        setItems(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoading(false);
+      });
+  }, []);
+
+  const options = {
+    loop: true,
+    margin: 15,
+    nav: true,
+    dots: false,
+    responsive: {
+      0:    { items: 1 },
+      560:  { items: 2 },
+      760:  { items: 3 },
+      1200: { items: 4 },
+    },
+  };
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,62 +45,50 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft__item">
-                <div className="author_list_pp">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
+          <div className="col-lg-12">
+            {loading ? (
+              <div className="row">
+                {Array(4).fill(null).map((_, index) => (
+                  <div className="col-lg-3 col-md-4 col-sm-6" key={index}>
+                    <div className="nft__item">
+                      <div className="author_list_pp">
+                        <div
+                          className="skeleton"
+                          style={{ width: "50px", height: "50px", borderRadius: "50%" }}
+                        />
+                      </div>
+                      <div className="nft__item_wrap">
+                        <div
+                          className="skeleton"
+                          style={{ width: "100%", height: "250px" }}
+                        />
+                      </div>
+                      <div className="nft__item_info">
+                        <div
+                          className="skeleton"
+                          style={{ width: "100px", height: "20px", marginBottom: "8px" }}
+                        />
+                        <div
+                          className="skeleton"
+                          style={{ width: "60px", height: "20px" }}
+                        />
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
+                ))}
               </div>
-            </div>
-          ))}
+            ) : (
+              <OwlCarousel className="owl-theme" {...options}>
+                {items.map((item) => (
+                <div key={item.id}>
+                <Items item={item} />
+                </div>
+                ))}
+              </OwlCarousel>
+            )}
+          </div>
+
         </div>
       </div>
     </section>
