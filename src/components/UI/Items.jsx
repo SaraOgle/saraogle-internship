f54f3489f782b3b4386d5a3198ed69cc09d21d4c
SaraOgle <259494087+SaraOgle@users.noreply.github.com>
2026-07-21 15:29:34 -0700

    fix

--- src/components/UI/Items.jsx
+++ src/components/UI/Items.jsx
@@ -1,4 +1,3 @@
-import React, {useState, useEffect} from 'react'
 import { Link } from "react-router-dom";
 import useCountdown from './CountdownTimer';
 
@@ -36,13 +35,13 @@ const Items = ({ item }) => {
           <button>Buy Now</button>
           <div className="nft__item_share">
             <h4>Share</h4>
-            <a href="" target="_blank" rel="noreferrer">
+            <a href="#" target="_blank" rel="noreferrer">
               <i className="fa fa-facebook fa-lg"></i>
             </a>
-            <a href="" target="_blank" rel="noreferrer">
+            <a href="#" target="_blank" rel="noreferrer">
               <i className="fa fa-twitter fa-lg"></i>
             </a>
-            <a href="">
+            <a href="#">
               <i className="fa fa-envelope fa-lg"></i>
             </a>
           </div>
